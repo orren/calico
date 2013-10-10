d68e56b88f0f4af274a715cb6b618b446f58d08e
orren <orren@seas.upenn.edu>
2013-10-10 15:21:03 -0400

    initial commit

---
+++ sample_methods.c
@@ -0,0 +1,32 @@
+/*
+ * sum(\multiply(A, 2), length) == \result * 2
+ * sum(\permute(A), length) == \result
+ */
+int sum(int A[], int length) {
+  int i, sum = 0;
+  for (i = 0; i < length; i++) {
+    sum += A[i];
+  }
+  return sum;
+}
+
+/*
+ * 
+ */
+int multiply(int A[], int factor, int length) {
+  int i;
+  for (i = 0; i < length; i++) {
+    A[i] *= factor;
+  }
+}
+
+int permute(int A[], int length) {
+  int i;
+  for (i = 0; i < length; i++) {
+    int which = random() % length;
+    int temp = A[i];
+    A[i] = A[which];
+    A[which] = temp;
+  }
+}
+
---
+++ test.c
@@ -0,0 +1,141 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+
+int __sum(int A[], int length) {
+  int i, sum = 0;
+  for (i = 0; i < length; i++) sum += A[i];
+  return sum;
+}
+
+int __multiply(int A[], int factor, int length) {
+  int i;
+  for (i = 0; i < length; i++) A[i] *= factor;
+}
+
+int __permute(int A[], int length) {
+  int i;
+  for (i = 0; i < length; i++) {
+    int which = random() % length;
+    int temp = A[i];
+    A[i] = A[which];
+    A[which] = temp;
+  }
+}
+
+/*
+ * sum(\multiply(A, 2), length) == \result * 2
+ */
+int __sum_test1(int A[], int length, int *result, char *ready) {
+  // apply transformation
+  __multiply(A, 2, length);
+    
+  int s = __sum(A, length); // call the function again
+
+  // wait for parent
+  while (*ready == 0);
+  
+  printf("I read %d\n", *result);
+  
+  // do the check
+  if (s == *result * 2) {
+    printf("Test 1 passed!\n");
+  }
+  else printf("Test 1 failed! %d %d\n", *result, s);
+  shmdt(result);
+  exit(0);
+}
+
+/*
+ * sum(\permute(A), length) == \result
+ */
+int __sum_test2(int A[], int length, int *result, char *ready) {
+  // apply transformation
+  __permute(A, length);
+    
+  int s = __sum(A, length); // call the function again
+
+  // wait for parent
+  while (*ready == 0);
+  
+  printf("I read %d\n", *result);
+  
+  // do the check
+  if (s == *result) {
+    printf("Test 2 passed!\n");
+  }
+  else printf("Test 2 failed! %d %d\n", *result, s);
+  shmdt(result);
+  exit(0);
+}
+
+int sum(int A[], int length) {
+
+  
+    int shmid;
+    key_t key = 9487; // set the key for the shared memory segment
+    int retval = 0;
+
+    int *result; // the type comes from the function's return value
+    char *ready; // to indicate that the result is ready
+
+    // Create the segment for the result
+    // the size is the same as the size of the function's return value
+    if ((shmid = shmget(key, sizeof(int), IPC_CREAT | 0666)) < 0) {
+        perror("shmget");
+        exit(1);
+    }
+    // Attach to the segment.
+    if ((result = shmat(shmid, NULL, 0)) ==  (int*)-1) {
+        perror("shmat");
+        exit(1);
+    }
+
+    // Create the segment for the ready flag
+    if ((shmid = shmget(key+1, sizeof(char), IPC_CREAT | 0666)) < 0) {
+        perror("shmget");
+        exit(1);
+    }
+    // Attach to the segment.
+    if ((ready = shmat(shmid, NULL, 0)) ==  (char*)-1) {
+        perror("shmat");
+        exit(1);
+    }
+    // initialize
+    *ready = 0;
+
+
+    if (fork() == 0) {
+      __sum_test1(A, length, result, ready);
+    }
+    if (fork() == 0) {
+      __sum_test2(A, length, result, ready);
+    }
+    
+    printf("I'm the parent\n");
+
+    retval = __sum(A, length); // original function call
+    
+    // put the return val in shared memory
+    *result = retval;
+    *ready = 1; // indicate that we've updated it
+
+    //printf("Rest of program\n");
+    return retval;
+}
+
+int main() {
+  
+  int array[] = { 4, 7, -2, 1, 0 };
+
+  int s = sum(array, 5);
+
+  printf("sum is %d\n", s);
+  
+}
