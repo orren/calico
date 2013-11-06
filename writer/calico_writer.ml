25589843ea651439fe21dd859a439495e0698b8c
orren <orren@seas.upenn.edu>
2013-11-06 17:30:46 -0500

    put writer in its own folder, split some things up

--- writer/calico_writer.ml
+++ writer/calico_writer.ml
@@ -1,43 +1,10 @@
 # load "str.cma" ;;
 open Printf
 open List
-
-type funKind = Pure | SideEffect | PointReturn
-
-type property = { input_prop: (string * funKind) list; output_prop: string * funKind }
-type parameter = { param_type: string; param_name: string }
-type annotatedFunction = { annotations: string; return_type: string; fun_kind: funKind; fun_name: string; parameters: parameter list; raw: string; properties: property list }
-type sourceUnderTest = { file_name: string; top_source: string list; functions: annotatedFunction list }
+# use "../SUT_struct.ml" ;;
+# use "mock_SUT.ml" ;;
 
 let key_number : string = "9847"
-
-let prop1 : property = {input_prop = [("multiply_int_array(A, 2, length)", SideEffect);
-                                      ("length", Pure)] ;
-                                      output_prop = ("multiply_int(result, 2)", Pure)}
-let prop2 : property = {input_prop = [("multiply_int_array(A, -1, length)", SideEffect);
-                                      ("length", Pure)] ;
-                                      output_prop = ("multiply_int(result, -1)", Pure)}
-
-let fun1 : annotatedFunction = {annotations = "/**\n * Sums the elements of an array?\n *\n * @input-prop multiply(A, 2, length), length\n * @output-prop multiply_int(result, 2)\n */";
-        return_type = "int"; fun_kind = Pure; fun_name = "sum"; 
-        parameters = [ {param_type = "int *"; param_name = "A";};
-                       {param_type = "int"; param_name = "length"} ] ;
-        raw = "(int *A, int length) {\n    int i, sum = 0;\n    for (i = 0; i < length; i++) sum += A[i];\n    return sum;\n}" ; properties = [prop1; prop2]}
-
-let prop3 : property = {input_prop = [("multiply_double(a, -1)", Pure)];
-                        output_prop = ("multiply_int(result, 2)", Pure)}
-
-let fun2 : annotatedFunction = {annotations = "/**\n * Returns a pointer to a double that is the absolute value of a\n *\n * @input-prop multiply_double(a, -1)\n * @output-prop result\n */";
-        return_type = "double *"; fun_kind = PointReturn; fun_name = "absolute";
-        parameters = [ {param_type = "double"; param_name = "a"} ] ;
-        raw = "(double a) {\n    double *answer = malloc(sizeof(int));\n    *answer = abs(a);\n    return answer;\n};"
-        ; properties = [prop3]}
-
-(* TODO: certain includes will always be necessary. We must check the first element of top_source to make sure those includes are already present, otherwise we must add them *)
-let simpleTestSUT : sourceUnderTest = {
-    file_name = "simple_test" ;
-    top_source = ["#include <unistd.h>\n#include <sys/types.h>\n#include <sys/ipc.h>\n#include <sys/shm.h>\n#include <stdio.h>\n#include <stdlib.h>\n#include <string.h>\n#include <math.h>"; "// some comment or whatever"] ;
-    functions = [fun1; fun2] }
 
 let rec repeat (s : string) (n : int) : string =
     if n <= 1 then s else s ^ (repeat s (n - 1))
