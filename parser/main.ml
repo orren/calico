orren/calico

commit ebb032369eed5aebe11e155a6a42a556a43ad942

use string_match instead of search_forward

parser/parsedriver.ml
@@ -18,12 +18,7 @@ let split_src (ac: annotated_comment) (src_str: string) : string =
 
 (* Test whether a comment string looks like an annotation
 *)
-let is_annot (com: string) : bool =
-  try
-    let _ = (search_forward (regexp "@fun-info") com 0) in
-    true
-  with
-      Not_found -> (* Printf.printf "Not an annotation : %s\n" com; *) false
+let is_annot (com: string) : bool = string_match (regexp "@fun-info") com 0
 
 (* Invokes the appropriate parser for each program element. Generates
  * an annotation/function pairs when possible.

parser/sum_example.c
@@ -17,7 +17,7 @@ extern void exit(int);
  * @output-prop { Pure } double_int(result, 1, 2, 3) ;
  */
 int sum ( int arr [], int length ) {
-  int i = 0, r = 0;
+  int i = 0, r = 0; /* mulit-line comment in source */
   for (; i < length;) r += arr[i++];
   return r;
 }

commit 6840b63de57d8e934534d6db0356983a35785b08

fix regression caused by ebb0323

parser/main.ml
@@ -3,8 +3,9 @@ open Parsedriver
 open Ast
 
 ;; let ic = (open_in
- (* "mut_example.c"
- "../martirank/merge.c" *) "sum_example.c") in
+               (* "mut_example.c" *)
+                  "../martirank/merge.c"
+                (* "sum_example.c" *)) in
    Printf.printf "Parse result: \n%s\n"
      (String.concat "\n"
         (List.map str_of_pelem (parse_file "merge.c" ic).elements));

parser/parsedriver.ml
@@ -18,7 +18,7 @@ let split_src (ac: annotated_comment) (src_str: string) : string =
 
 (* Test whether a comment string looks like an annotation
 *)
-let is_annot (com: string) : bool = string_match (regexp "@fun-info") com 0
+let is_annot (com: string) : bool = string_match (regexp "\(\n\|.\)*@fun-info\(\n\|.\)*") com 0
 
 (* Invokes the appropriate parser for each program element. Generates
  * an annotation/function pairs when possible.